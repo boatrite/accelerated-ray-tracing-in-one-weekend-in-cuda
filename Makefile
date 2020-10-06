cc80c4206a162f4afb2b2ec7da314e7770de8651
Gwen Boatrite <gwen.boatrite@gmail.com>
2020-10-06 18:50:19 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,9 @@
+all: build/my_program
+	bash -c "display <(build/my_program)"
+
+build/my_program: src/main.cu
+	mkdir -p build
+	nvcc src/main.cu -o build/my_program
+
+clean:
+	rm -r build
---
+++ src/main.cu
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <time.h>
+
+#include "common.h"
+#include "vec3.h"
+
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+  if (result) {
+    std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+      file << ":" << line << " '" << func << "' \n";
+    // Make sure we call CUDA Device Reset before exiting
+    cudaDeviceReset();
+    exit(99);
+  }
+}
+
+__global__ void render(vec3 *fb, int max_x, int max_y) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  int j = threadIdx.y + blockIdx.y * blockDim.y;
+  if((i >= max_x) || (j >= max_y)) return;
+  int pixel_index = j*max_x + i;
+  fb[pixel_index] = vec3(float(i) / max_x, float(j) / max_y, 0.2);
+}
+
+int main() {
+  int nx = 1200;
+  int ny = 600;
+  int tx = 8;
+  int ty = 8;
+
+  std::cerr << "Rendering a " << nx << "x" << ny << " image ";
+  std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+
+  int num_pixels = nx*ny;
+  size_t fb_size = num_pixels*sizeof(vec3);
+
+  // allocate FB
+  vec3 *fb;
+  checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
+
+  clock_t start, stop;
+  start = clock();
+  // Render our buffer
+  dim3 blocks(nx/tx+1,ny/ty+1);
+  dim3 threads(tx,ty);
+  render <<<blocks, threads>>>(fb, nx, ny);
+  checkCudaErrors(cudaGetLastError());
+  checkCudaErrors(cudaDeviceSynchronize());
+  stop = clock();
+  double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+  std::cerr << "took " << timer_seconds << " seconds.\n";
+
+  // Output FB as Image
+  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  for (int j = ny-1; j >= 0; j--) {
+    for (int i = 0; i < nx; i++) {
+      size_t pixel_index = j*nx + i;
+      float r = fb[pixel_index].r();
+      float g = fb[pixel_index].g();
+      float b = fb[pixel_index].b();
+      int ir = int(255.99*r);
+      int ig = int(255.99*g);
+      int ib = int(255.99*b);
+      std:: cout << ir << " " << ig << " " << ib << "\n";
+    }
+  }
+
+  checkCudaErrors(cudaFree(fb));
+}
