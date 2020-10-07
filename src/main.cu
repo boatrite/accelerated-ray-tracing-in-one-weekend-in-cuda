f17bb252db3b6f2326dc8be3acd81b1e0dbce4f0
Gwen Boatrite <gwen.boatrite@gmail.com>
2020-10-06 19:26:27 -0500

    Add sphere

--- src/main.cu
+++ src/main.cu
@@ -14,7 +14,18 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
   }
 }
 
+__device__ bool hit_sphere(const point3& center, double radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0f * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius*radius;
+  auto disc = b*b - 4.0f*a*c;
+  return (disc > 0);
+}
+
 __device__ vec3 ray_color(const ray& r) {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    return color(1,0,0);
   vec3 unit_direction = unit_vector(r.direction());
   float t = 0.5f * (unit_direction.y() + 1.0f);
   return (1.0f - t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
