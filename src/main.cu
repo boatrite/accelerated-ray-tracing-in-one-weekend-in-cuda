a44f00b28743e41986476814cfabe0a830d343ca
Gwen Boatrite <gwen.boatrite@gmail.com>
2020-10-06 19:23:12 -0500

    Add ray class

--- src/main.cu
+++ src/main.cu
@@ -14,24 +14,47 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
   }
 }
 
-__global__ void render(vec3 *fb, int max_x, int max_y) {
+__device__ vec3 ray_color(const ray& r) {
+  vec3 unit_direction = unit_vector(r.direction());
+  float t = 0.5f * (unit_direction.y() + 1.0f);
+  return (1.0f - t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
+__global__ void render(vec3 *fb, int max_x, int max_y, vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int j = threadIdx.y + blockIdx.y * blockDim.y;
   if((i >= max_x) || (j >= max_y)) return;
   int pixel_index = j*max_x + i;
-  fb[pixel_index] = vec3(float(i) / max_x, float(j) / max_y, 0.2);
+
+  float u = float(i) / float(max_x);
+  float v = float(j) / float(max_y);
+  ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+  fb[pixel_index] = ray_color(r);
 }
 
 int main() {
-  int nx = 1200;
-  int ny = 600;
+  // Image
+  const float aspect_ratio = 16.0f / 9.0f;
+  const int image_width = 400;
+  const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+  // Camera
+  float viewport_height = 2.0f;
+  float viewport_width = aspect_ratio * viewport_height;
+  float focal_length = 1.0f;
+
+  auto origin = point3(0.0f, 0.0f, 0.0f);
+  auto horizontal = vec3(viewport_width, 0.0f, 0.0f);
+  auto vertical = vec3(0.0f, viewport_height, 0.0f);
+  auto lower_left_corner = origin - horizontal/2.0f - vertical/2.0f - vec3(0.0f, 0.0f, focal_length);
+
   int tx = 8;
   int ty = 8;
 
-  std::cerr << "Rendering a " << nx << "x" << ny << " image ";
+  std::cerr << "Rendering a " << image_width << "x" << image_height << " image ";
   std::cerr << "in " << tx << "x" << ty << " blocks.\n";
 
-  int num_pixels = nx*ny;
+  int num_pixels = image_width*image_height;
   size_t fb_size = num_pixels*sizeof(vec3);
 
   // allocate FB
@@ -41,9 +64,9 @@ int main() {
   clock_t start, stop;
   start = clock();
   // Render our buffer
-  dim3 blocks(nx/tx+1,ny/ty+1);
+  dim3 blocks(image_width/tx+1,image_height/ty+1);
   dim3 threads(tx,ty);
-  render <<<blocks, threads>>>(fb, nx, ny);
+  render <<<blocks, threads>>>(fb, image_width, image_height, lower_left_corner, horizontal, vertical, origin);
   checkCudaErrors(cudaGetLastError());
   checkCudaErrors(cudaDeviceSynchronize());
   stop = clock();
@@ -51,10 +74,10 @@ int main() {
   std::cerr << "took " << timer_seconds << " seconds.\n";
 
   // Output FB as Image
-  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-  for (int j = ny-1; j >= 0; j--) {
-    for (int i = 0; i < nx; i++) {
-      size_t pixel_index = j*nx + i;
+  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+  for (int j = image_height-1; j >= 0; j--) {
+    for (int i = 0; i < image_width; i++) {
+      size_t pixel_index = j*image_width + i;
       float r = fb[pixel_index].r();
       float g = fb[pixel_index].g();
       float b = fb[pixel_index].b();
